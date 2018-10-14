e0162388b356e64fe6d3bb8ed820eb356236c219
jomer14 <jomer14@mail.wou.edu>
2018-10-14 16:52:01 -0700

    Added LinkedQueue and Main

---
+++ HW3/HW3/HW3/LinkedQueue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class LinkedQueue<T> : IQueueInterface<T>
+{
+
+    private Node<T> front;
+
+    private Node<T> rear;
+
+    public LinkedQueue()
+    {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public T push(T element)
+    {
+        if ((element == null))
+        {
+            throw new NullPointerException();
+        }
+
+        if (this.isEmpty())
+        {
+            Node<T> tmp = new Node<T>(element, null);
+            this.front = tmp;
+            this.rear = tmp;
+        }
+        else
+        {
+            //  General case
+            Node<T> tmp = new Node<T>(element, null);
+            this.rear.next = tmp;
+            this.rear = tmp;
+        }
+
+        return element;
+    }
+
+    public T pop()
+    {
+        T tmp = null;
+        if (this.isEmpty())
+        {
+            throw new QueueUnderflowException("The queue was empty when pop was invoked.");
+        }
+        else if ((this.front == this.rear))
+        {
+            //  one item in queue
+            tmp = this.front.data;
+            this.front = null;
+            this.rear = null;
+        }
+        else
+        {
+            //  General case
+            tmp = this.front.data;
+            this.front = this.front.next;
+        }
+
+        return tmp;
+    }
+
+    public bool isEmpty()
+    {
+        if (((this.front == null)
+                    && (this.rear == null)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
---
+++ HW3/HW3/HW3/Main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+public class Main
+{
+
+    static LinkedList<String> generateBinaryRepresentationList(int n)
+    {
+        //  Create an empty queue of strings with which to perform the traversal
+        LinkedQueue<StringBuilder> q = new LinkedQueue<StringBuilder>();
+
+        //  A list for returning the binary values
+        LinkedList<String> output = new LinkedList<String>();
+        if ((n < 1))
+        {
+            //  binary representation of negative values is not supported
+            //  return an empty list
+            return output;
+        }
+
+        //  Enqueue the first binary number.  Use a dynamic string to avoid string concat
+        q.push(new StringBuilder("1"));
+
+        //  BFS 
+        while (n-1> 0)
+        {
+            //  print the front of queue 
+            StringBuilder sb = q.pop();
+            output.add(sb.toString());
+
+            //  Make a copy
+            StringBuilder sbc = new StringBuilder(sb.toString());
+
+            //  Left child
+            sb.append('0');
+            q.push(sb);
+
+            //  Right child
+            sbc.append('1');
+            q.push(sbc);
+        }
+
+        return output;
+    }
+
+    //  Driver program to test above function 
+    public static void main(String[] args)
+    {
+        int n = 10;
+        if ((args.length < 1))
+        {
+            Console.WriteLine("Please invoke with the max value to print binary up to, like this:");
+            Console.WriteLine("\tjava Main 12");
+            return;
+        }
+
+        try
+        {
+            n = Integer.parseInt(args[0]);
+        }
+        catch (NumberFormatException e)
+        {
+            Console.WriteLine(("I\'m sorry, I can\'t understand the number: " + args[0]));
+            return;
+        }
+
+        LinkedList<String> output = Main.generateBinaryRepresentationList(n);
+
+        //  Print it right justified.  Longest string is the last one.
+        //  Print enough spaces to move it over the correct distance
+        int maxLength = output.getLast().length();
+        foreach (String s in output)
+        {
+            for (int i = 0; (i
+                        < (maxLength - s.length())); i++)
+            {
+                Console.WriteLine(" ");
+            }
+
+            Console.WriteLine(s);
+        }
+
+    }
+}
